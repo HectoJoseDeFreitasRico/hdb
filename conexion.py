fb2dbe148b5cd125b2cc8ae981d2d020d065b176
HectoJoseDeFreitasRico <hjfnogueira@gmail.com>
2024-02-02 02:09:13 +0100

    Parametrización

--- conexion.py
+++ conexion.py
@@ -1,7 +1,12 @@
 import subprocess
 
-comando = '"C:\\Users\hjfno\\Documents\\GitHub\\hdb\\hdb.exe" insert miempresa clientes cliente4 "Este es el cliente 4"'
+operacion = "insert"
+basededatos = "miempresa"
+coleccion = "clientes"
+documento = "cliente5"
+contenido = "este es otro contenido de prueba"
 
+comando = '"C:\\Users\hjfno\\Documents\\GitHub\\hdb\\hdb.exe" '+operacion+' '+basededatos+' '+coleccion+' '+documento+' "'+contenido+'"'
 resultado = subprocess.run(comando,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True)
 
 if resultado.returncode == 0:
