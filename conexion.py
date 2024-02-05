7bc67608502e38ce9495ebf2380a359a3a9b56c5
= <hjfnogueira@gmail.com>
2024-02-06 00:06:52 +0100

    a

--- conexion.py
+++ conexion.py
@@ -1,4 +1,4 @@
 from hdb_conector import Hdb
 
 Conexion1 = Hdb("miempresa")
-Conexion1.insert("clientes","cliente9","Este es otro contenido de prueba")
+Conexion1.insert("clientes","cliente10","Este es otro contenido de prueba")
